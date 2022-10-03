abb547f84685a82da5dfdbc21f9570c49a02433a
Anna <annechkap@list.ru>
2022-10-03 22:49:30 +0300

    Практическое задание.Задача4

---
+++ 004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count<=n)
+{
+       Console.WriteLine(count);
+       count=count+2; 
+}
+
+
