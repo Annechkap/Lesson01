f93eef1d3b72bd76b0bd68fb8fe118a84250cb64
Anna <annechkap@list.ru>
2022-10-03 21:25:42 +0300

    Практическое задание.Задача1

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше
+
+Console.WriteLine("Введите первое число:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"Первое число больше второго");
+}
+else if ( a< b)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+else
+{
+     Console.WriteLine("Оба числа равны");
+}
